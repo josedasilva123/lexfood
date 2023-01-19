636cf6b6ab98f4eb670a31e23e42200009784111
Alex Conder <alex.v.conder@gmail.com>
2023-01-19 08:57:09 -0300

    feat: featLoading svg

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyledLoading } from './style'
+import LoadingSVG from "../../assets/Loading.svg";
+
+const Loading = () => {
+  return (
+    <StyledLoading>
+        <LoadingSVG />
+    </StyledLoading>    
+  )
+}
+
+export default Loading
---
+++ src/components/Loading/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledLoading = styled.div`
+    width: 100%;
+    height: calc(100vh - 90px);
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
