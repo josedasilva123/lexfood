f291a0ded94f96a187059a3366f4ee6ceca819f1
Alex Conder <alex.v.conder@gmail.com>
2023-01-19 08:58:21 -0300

    feat: starting recipe single page

--- src/pages/RecipePage/RecipeSinglePage/index.tsx
+++ src/pages/RecipePage/RecipeSinglePage/index.tsx
@@ -1,15 +1,38 @@
 import React from 'react'
+import { useQuery } from 'react-query'
 import { useParams } from 'react-router-dom'
+import { api } from '../../../api/api'
 import Header from '../../../components/Header'
+import { iRecipe } from '../../../providers/RecipeContext/@types'
 import { StyledContainer } from '../../../styles/grid'
+
+interface iRecipeGetResponse{
+  recipe: iRecipe;
+}
 
 const RecipeSinglePage = () => {
   const { recipeId } = useParams();
+  const { data: recipe, isLoading: recipeLoading } = useQuery({
+    queryKey: ['recipe'],
+    queryFn: async () => {
+      try {
+        const token = localStorage.getItem("@TOKEN");
+        const response = await api.get<iRecipeGetResponse>(`recipe/${recipeId}`, {
+          headers: {
+            auth: token
+          }
+        });
+        return response.data.recipe;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  })
   return (
     <div>
       <Header />
-      <StyledContainer>
-        {}
+      <StyledContainer containerWidth={800}>
+        {recipeId}
       </StyledContainer>
         
     </div>
