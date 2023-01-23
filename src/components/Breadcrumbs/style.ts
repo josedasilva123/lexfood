625c41099a8cbb90376f14980b7d45973c28eff8
Alex Conder <alex.v.conder@gmail.com>
2023-01-23 08:34:36 -0300

    fix: fixing visual responsive issue in breadcrumbs

--- src/components/Breadcrumbs/style.ts
+++ src/components/Breadcrumbs/style.ts
@@ -4,10 +4,11 @@ import styled from "styled-components";
 export const StyledBreadcrumbs = styled.p`
    display: flex;
    align-items: center;
+   flex-wrap: wrap;
    gap: 8px;
    padding: 20px 0;
 
-   color: ${({ theme }) => theme.colors.orange};
+   color: ${({ theme }) => theme.colors.lockOrange};
 
    .currentPage {
       font-weight: 600;
@@ -18,12 +19,13 @@ export const StyledBreadcrumbs = styled.p`
       display: inline-flex;
       align-items: center;
       gap: 8px;
+      white-space: nowrap;
    }
 `;
 
 export const StyledBreadcrumbsLink = styled(Link)`
-   color: ${({ theme }) => theme.colors.orange};
-
+   color: ${({ theme }) => theme.colors.lockOrange};
+   white-space: nowrap;
    &:hover {
       text-decoration: underline;
    }
